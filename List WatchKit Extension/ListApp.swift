ba6904a6a2364cdaeeadee67c7e11c38808b2cbc
Chris Breiding <chrisbreiding@gmail.com>
2021-05-28 17:43:08 -0400

    Initial Commit

---
+++ List WatchKit Extension/ListApp.swift
@@ -0,0 +1,19 @@
+//
+//  ListApp.swift
+//  List WatchKit Extension
+//
+//  Created by Chris Breiding on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
